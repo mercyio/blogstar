6512218fdb1f5d46b3b6efcfe543ff6350e1cb85
mercy vincent <mercydanke@gmail.com>
2025-01-01 19:43:05 +0100

    hook in to mongoose query generation and execution process

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,14 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('about to run a query');
+
+    console.log(this.getQuery())
+    console.log(this.mongooseCollection.name)
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+
+    console.log(key);
+
     return exec.apply(this, arguments);
 };
