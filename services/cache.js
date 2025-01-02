e054f26cb49a05ea4cfef43170a8f5f4217e3bb5
mercy vincent <mercydanke@gmail.com>
2025-01-02 20:03:39 +0100

    setting up a more reuseable caching solution

--- services/cache.js
+++ services/cache.js
@@ -1,17 +1,34 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
     console.log('about to run a query');
 
     console.log(this.getQuery())
     console.log(this.mongooseCollection.name)
-    const key = Object.assign({}, this.getQuery(), {
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
-    });
+    }));
 
     console.log(key);
 
-    return exec.apply(this, arguments);
+    // see if we have a value for key in redis=_t
+    const cacheValue = await client.get(key);
+
+
+    // if we do, return that
+    if (cacheValue) {
+    console.log(cacheValue)
+    }
+
+    // otherwise issue the query and store the result in redis
+
+    const result = await exec.apply(this, arguments);
+    console.log(result);
 };
