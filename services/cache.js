6babc88bfda4a93b75d69b699c6de658409cbb25
mercy vincent <mercydanke@gmail.com>
2025-01-03 20:53:05 +0100

    setting up cache helper function

--- services/cache.js
+++ services/cache.js
@@ -7,11 +7,18 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true
+    return this;
+}
 mongoose.Query.prototype.exec = async function () {
     console.log('about to run a query');
-
     console.log(this.getQuery())
     console.log(this.mongooseCollection.name)
+
+    if (!this.useCache) {
+        return exec.apply(this, arguments)
+    };
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
