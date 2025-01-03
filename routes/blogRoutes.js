18f6eb6f06a0c6c46eb9b9b82599444157ae9c1a
mercy vincent <mercydanke@gmail.com>
2025-01-03 18:06:30 +0100

    handle redis cache to store and fetch the loggedin user blog posts

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,6 +14,23 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util')
+    //  promisify : we can pass it into another function and it will return a new function
+
+     // so we promisify the client.get function to return a new function instead of using the callback function method
+
+    client.get = util.promisify(client.get);
+    
+
+    // check if we cached data in redis related to this query. if yes, then response to this request immediately
+    const cacheBlog =  await client.get(req.user.id);
+
+   
+    // if no we need to respond to request and update our cache to store the data
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
