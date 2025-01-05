e49606105c4511fa1ccdffbe8f6ab25ee5166930
mercy vincent <mercydanke@gmail.com>
2025-01-05 23:39:17 +0100

    clear the data from redis that is tied to a users post when a user makes a new post, then resave all the users post in the redis

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,15 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
-    this.useCache = true
+mongoose.Query.prototype.cache = function (
+    // options = {} || ""
+) {
+    this.useCache = true;
+    this._hashKeyToUse = JSON.stringify(options.key);
+
     return this;
 }
 mongoose.Query.prototype.exec = async function () {
@@ -48,3 +52,11 @@ mongoose.Query.prototype.exec = async function () {
     client.set(key, JSON.stringify(result));
     console.log(result);
 };
+
+// to delete all the info associated with the given key in the redis
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey))
+    }
+  }
+  
